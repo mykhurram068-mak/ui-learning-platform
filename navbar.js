b57a694602305a3531c87e45ab1ffcea48d44b02
MAK <mykhurram068@gmail.com>
2026-08-09 22:45:34 +0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -28,6 +28,7 @@
             <li><a href="loader.html">⏳ Loaders</a></li>
             <li><a href="login.html">🔐 Login Form</a></li>
             <li><a href="darkmode.html">🌙 Dark Mode</a></li>
+            <li><a href="image-slider.html">↔️ Image Slider</a></li>
             <li><a href="navbar-snippet.html">📱 Navbar</a></li>
             <li><a href="password-gen.html">🔐 Password Generator</a></li>
             <li><a href="ai-analyser.html">💬 Sentiment Analyser</a></li>
