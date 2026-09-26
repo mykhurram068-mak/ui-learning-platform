129e437039b32ce373a6cef2558c9cdb53dae7f4
MAK <mykhurram068@gmail.com>
2026-09-26 18:21:02 +0500

    Add link to 'Publish Website Free' in navbar

--- navbar.js
+++ navbar.js
@@ -61,6 +61,7 @@
             <li><a href="/how-to/command-line-basics.html">⌨️ Command Line Basics</a></li>
             <li><a href="/how-to/write-first-html-page.html">📝 Write First HTML Page</a></li>
             <li><a href="/how-to/install-git.html">🌿 Install Git</a></li>
+            <li><a href="/how-to/publish-website-free.html">🚀 Publish Website Free</a></li>
             <!--<li><a href="ultimate-ai-guide.html">🤖 Ultimate AI Guide</a></li>
             <li><a href="#">⚡ JavaScript</a>⌨️</li>-->
           </ul>
